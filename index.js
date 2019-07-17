888aec0cbe36bfbec91e38741792ffbefb2ccf0e
Jed Datu <jeddatu@TRAdmins-MacBook-Pro-2.local>
2019-07-17 16:27:18 +0800

    Completes code from tutorial.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const router = require('./router')
 const mongoose =require('mongoose')
+const cors = require('cors')
 
 // DB setup
 mongoose.connect(
@@ -17,6 +18,7 @@ mongoose.connect(
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json({ type: '*/*' }))
+app.use(cors())
 router(app)
 
 // Server setup
